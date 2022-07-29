b9527f20c7fbfaa7eac74fc89597a179a07083b7
Cairo Filho <cairoosf@gmail.com>
2022-07-28 23:49:50 -0300

    implantação do mockery para validação de cobrança do frete no valor total do carrinho

--- app/Business/Infra/DeliveryService/DeliveryServiceEloquentRepository.php
+++ app/Business/Infra/DeliveryService/DeliveryServiceEloquentRepository.php
@@ -2,15 +2,21 @@
 
 namespace App\Business\Infra\DeliveryService;
 
+use App\Business\Infra\ServiceType\Correios\CorreiosInterface;
+use App\Models\Cart;
 use App\Models\DeliveryService;
+use App\Models\Product;
+use App\Models\User;
 
 class DeliveryServiceEloquentRepository
 {
     protected $model;
+    protected $service;
 
-    public function __construct()
+    public function __construct(CorreiosInterface $service)
     {
         $this->model = new DeliveryService();
+        $this->service = $service;
     }
 
     public function all(): array
@@ -20,9 +26,12 @@ class DeliveryServiceEloquentRepository
 
     public function create(array $data): ?object
     {
+        if($this->findByCart($data['cart_id'])){
+            return null;
+        }
         $this->model->fill($data);
         $this->model->save();
-        
+
         return $this->model;
     }
 
@@ -33,11 +42,30 @@ class DeliveryServiceEloquentRepository
 
     public function findByCart(int $cart): ?object
     {
-        return $this->model->where('cart_id',$cart)->first();
+        return $this->model->where('cart_id', $cart)->first();
     }
 
     public function deleteById(int $id): bool
     {
         return $this->model->find($id)->delete();
     }
+
+    public function getTotal(Cart $cart): float
+    {
+        $sum = 0;
+        foreach ($cart->items()->get() as $item) {
+            $sum += Product::find($item->product_id)->value * $item->quantity;
+        }
+        return $sum;
+    }
+
+    public function getTotalFee(Cart $cart): float
+    {
+        $sum = $this->getTotal($cart);
+        if ($sum < 100) {
+            $sum +=  $this->service->findCostByZipCode(User::find($cart->user_id)->address);
+        }
+        return $sum;
+    }
+
 }
--- tests/Unit/App/Business/Infra/DeliveryServiceTest.php
+++ tests/Unit/App/Business/Infra/DeliveryServiceTest.php
@@ -2,10 +2,16 @@
 
 namespace Tests\Feature;
 
+use App\Business\Infra\Cart\CartEloquentRepository;
 use App\Business\Infra\DeliveryService\DeliveryServiceEloquentRepository;
+use App\Business\Infra\ServiceType\Correios\CorreiosInterface;
+use App\Business\Infra\ServiceType\Correios\CorreiosRepository;
+use App\Models\Cart;
 use App\Models\DeliveryService;
+use App\Models\Product;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Mockery;
 use Tests\TestCase;
 
 class DeliveryServiceTest extends TestCase
@@ -15,8 +21,17 @@ class DeliveryServiceTest extends TestCase
 
     protected function setUp(): void
     {
-        $this->repository = new DeliveryServiceEloquentRepository();
+        $service = Mockery::mock(CorreiosRepository::class,CorreiosInterface::class);
+        $service->shouldReceive("findCostByZipCode")->andReturn(30.00);
+
+        $this->repository = new DeliveryServiceEloquentRepository($service);
         parent::setUp();
+    }
+
+    protected function tearDown(): void
+    {
+        Mockery::close();
+        parent::tearDown();
     }
 
     public function test_all()
@@ -28,23 +43,59 @@ class DeliveryServiceTest extends TestCase
 
     public function test_create()
     {
-        $cartItem01 = $this->repository->create(DeliveryService::factory()->make()->getAttributes());
+        $deliveryService01 = $this->repository->create(DeliveryService::factory()->make()->getAttributes());
 
-        $this->assertNotEmpty($cartItem01->id);
+        $this->assertNotEmpty($deliveryService01->id);
     }
 
     public function test_find_by_id()
     {
-        $cartItem01 = $this->repository->create(DeliveryService::factory()->make()->getAttributes());
+        $deliveryService01 = $this->repository->create(DeliveryService::factory()->make()->getAttributes());
 
-        $this->assertEquals($cartItem01->id, $this->repository->findById($cartItem01->id)->id);
+        $this->assertEquals($deliveryService01->id, $this->repository->findById($deliveryService01->id)->id);
     }
 
     public function test_delete_by_id()
     {
-        $cartItem01 = $this->repository->create(DeliveryService::factory()->make()->getAttributes());
-        $this->repository->deleteById($cartItem01->id);
+        $deliveryService01 = $this->repository->create(DeliveryService::factory()->make()->getAttributes());
+        $this->repository->deleteById($deliveryService01->id);
         
-        $this->assertEmpty($this->repository->findById($cartItem01->id));
+        $this->assertEmpty($this->repository->findById($deliveryService01->id));
+    }
+
+    public function test_cart_is_empty()
+    {
+        $cart = Cart::factory()->create();
+        $this->repository->create(DeliveryService::factory()->make(['cart_id'=>$cart->id])->getAttributes());
+
+        $this->assertEquals(0,$this->repository->getTotal($cart));
+    }
+
+    public function test_check_total_cart_service()
+    {
+        
+        $cartRepository = new CartEloquentRepository();        
+        $cart01 = $cartRepository->create(Cart::factory()->make()->getAttributes());
+
+        $cartRepository->addProduct($cart01->id,Product::factory()->create(['value'=>140]));
+
+        $this->repository->create(DeliveryService::factory()->make(['cart_id'=>$cart01->id])->getAttributes());
+
+
+        $this->assertEquals(140,$this->repository->getTotalFee($cart01));
+    }
+
+    public function test_check_total_fee_cart_service()
+    {
+        
+        $cartRepository = new CartEloquentRepository();        
+        $cart01 = $cartRepository->create(Cart::factory()->make()->getAttributes());
+
+        $cartRepository->addProduct($cart01->id,Product::factory()->create(['value'=>80]));
+
+        $this->repository->create(DeliveryService::factory()->make(['cart_id'=>$cart01->id])->getAttributes());
+
+
+        $this->assertEquals(110,$this->repository->getTotalFee($cart01));
     }
 }
