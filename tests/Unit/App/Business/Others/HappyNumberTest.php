4a9be3ca2b5e46696db9f566df1bbf65de0ed400
Cairo Filho <cairoosf@gmail.com>
2022-07-29 14:35:33 -0300

    Aplicando o padrão Arrange Act Assert

--- tests/Unit/App/Business/Others/HappyNumberTest.php
+++ tests/Unit/App/Business/Others/HappyNumberTest.php
@@ -11,19 +11,28 @@ class HappyNumberTest extends TestCase
     public function test_if_value_is_happy_number()
     {
         $happyNumber = new HappyNumber();
-        $this->assertEquals($happyNumber->isHappyNumber(7),true);
+        
+        $isHappyNumber = $happyNumber->isHappyNumber(7);
+
+        $this->assertTrue($isHappyNumber);
     }
     
     public function test_if_value_is_not_happy_number()
     {
         $happyNumber = new HappyNumber();
-        $this->assertEquals($happyNumber->isHappyNumber(9),false);
+
+        $isHappyNumber = $happyNumber->isHappyNumber(9);
+
+        $this->assertFalse($isHappyNumber);
     }    
     
     public function test_histories_verification()
     {
         $happyNumber = new HappyNumber();
+        
         $happyNumber->isHappyNumber(7);
-        $this->assertEquals(array_diff([7,49,97,130,10],$happyNumber->getHistories()),[]);
+        $historiesVerification = array_diff([7,49,97,130,10],$happyNumber->getHistories());
+
+        $this->assertEmpty($historiesVerification);
     }
 }
